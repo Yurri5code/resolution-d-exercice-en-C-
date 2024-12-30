5037310d9f53a885191f687af338697156fb8eb6
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-12-30 23:54:02 +0300

    Create main.c

---
+++ chapter 1/probleme 6/main.c
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int sum_proper_divisors(int const number ){
+    int result = 1;
+    for(int i = 2;i <= sqrt(number);i++){
+        if(number %i == 0){
+            result += (i == (number / i)) ? i : (i + number / i);
+        }
+    }
+    return result;
+}
+
+void print_abundant(int const limit){
+    for(int number = 10; number <= limit;++number){
+        auto sum = sum_proper_divisors(number);
+        if(sum > number){
+            cout <<number <<" , abundance = "<<sum - number<<endl;
+        }
+    }
+}
+
+int main()
+{
+    int limit = 0;
+    cout <<" Upper limit : ";
+    cin >> limit;
+    print_abundant(limit);
+    return 0;
+}
